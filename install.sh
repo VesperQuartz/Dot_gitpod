3c069534e36b075757843d05097b0058337c7143
mrlectus <lilbrown3000@gmail.com>
2024-11-26 02:34:52 +0100

    ----

--- install.sh
+++ install.sh
@@ -12,7 +12,13 @@ chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract >/dev/null
 mkdir -p /home/gitpod/.local/bin
 ln -s "$(pwd)/squashfs-root/AppRun" /home/gitpod/.local/bin/nvim
-
+(
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  sudo install lazygit -D -t /usr/local/bin/
+) >/dev/null &
+disown
 (
   sudo apt update
   sudo apt install -y neofetch \
